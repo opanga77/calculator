836d823710233e135d3200aafc7ff9678e69d257
opanga77 <andersoking77@gmail.com>
2020-02-20 18:06:25 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
-//by js without jquery// 
+ 
  function multiplyBy()
 {
         num1 = document.getElementById("firstNumber").value;
         num2 = document.getElementById("secondNumber").value;
         document.getElementById("result").innerHTML = num1 * num2;
 }
-
 function divideBy() 
 { 
         num1 = document.getElementById("firstNumber").value;
         num2 = document.getElementById("secondNumber").value;
 document.getElementById("result").innerHTML = num1 / num2;
 }
-
+function subtractBy() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = num1 - num2;
+}
+function additionBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1+num2;
+}
